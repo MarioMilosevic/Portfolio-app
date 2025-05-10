306b1bedb67f9f4647a29f7b1262f7a169a18305
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2025-05-10 21:52:53 +0200

    Add hero sectioin

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Navigation from "src/components/ui/Navigation";
+import Hero from "./components/ui/Hero";
 
 function App() {
   return (
     <>
       <Navigation />
+      <Hero/>
     </>
   );
 }
--- src/components/ui/Navigation.tsx
+++ src/components/ui/Navigation.tsx
@@ -2,12 +2,12 @@
 const Header = () => {
   return (
     <nav className="border-b border-b-slate-600 fixed w-full h-16 flex justify-center gap-2 py-2">
-      <button className="btn bg-slate-700">HOME</button>
-      <button className="btn">ABOUT</button>
-      <button className="btn">EXPERIENCE</button>
-      <button className="btn">PROJECTS</button>
-      <button className="btn">RECOMMENDATIONS</button>
-      <button className="btn">EXPLORE</button>
+      <button className="btn btn-soft btn-primary">HOME</button>
+      <button className="btn btn-soft btn-primary">ABOUT</button>
+      <button className="btn btn-soft btn-primary">EXPERIENCE</button>
+      <button className="btn btn-soft btn-primary">PROJECTS</button>
+      <button className="btn btn-soft btn-primary">RECOMMENDATIONS</button>
+      <button className="btn btn-soft btn-primary">EXPLORE</button>
     </nav>
   );
 };
