44935d3a96e51d8806f50be2786a0f0ba0074498
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2025-05-25 12:13:17 +0200

    Make project responsive

--- src/components/ui/recommendations/Recommendation.tsx
+++ src/components/ui/recommendations/Recommendation.tsx
@@ -13,7 +13,7 @@ const Recommendation = ({ recommendation }: RecommendationProps) => {
   const iconSize = 22;
 
   return (
-    <div className="relative border-l-4 pl-12 pr-8 py-4 mb-12 min-h-[200px] flex flex-col gap-8">
+    <div className="relative border-l-4 md:pl-12 px-8 md:pr-8 py-4 mb-12 flex flex-col md:gap-8 gap-4">
       <span className="absolute top-0 left-2 text-5xl">“</span>
       <p className="text-justify text-sm">{text}</p>
       <div className="flex justify-between items-end">
@@ -22,11 +22,11 @@ const Recommendation = ({ recommendation }: RecommendationProps) => {
             <img src={image} alt={image} />
           </div>
           <span className="flex flex-col gap-1">
-            <p className="font-semibold">{fullName}</p>
-            <p className="font-semibold">{role}</p>
+            <p className="font-semibold md:text-base text-sm">{fullName}</p>
+            <p className="font-semibold md:text-base text-sm">{role}</p>
           </span>
         </div>
-        <div className="flex gap-4">
+        <div className="flex md:gap-4 gap-2">
           {github && (
             <Icon size="small" path={github}>
               <FaGithub size={iconSize} />
